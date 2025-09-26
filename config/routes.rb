30a72eb0a786cd3aa468ce9cba6296a779c972a2
RV <rock1990vishal@gmail.com>
2025-09-26 17:42:44 +0530

    temporarily disable action cable

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   # Action Cable
-  mount ActionCable.server => '/cable'
+  # mount ActionCable.server => '/cable'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
